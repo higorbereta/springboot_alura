f1fa466828cda3496f73811e926ce356a3418a9d
Higor Bereta <higor.bereta@gmail.com>
2023-05-12 16:59:04 -0300

    Cadastro medico e paciente

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,12 @@
+package med.voll.api.endereco;
+
+public class Endereco {
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+
+}
