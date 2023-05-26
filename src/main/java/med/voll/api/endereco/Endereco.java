97aacf5623110e95d16d0a10e7922ed3b7689c79
Higor Bereta <higor.bereta@gmail.com>
2023-05-26 10:40:13 -0300

    Cadastro medico e paciente

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -1,5 +1,12 @@
 package med.voll.api.endereco;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Endereco {
     private String logradouro;
     private String numero;
@@ -9,4 +16,13 @@ public class Endereco {
     private String cidade;
     private String uf;
 
+    public Endereco(DadosEndereco dadosEndereco) {
+        this.logradouro = dadosEndereco.logradouro();
+        this.numero = dadosEndereco.numero();
+        this.complemento = dadosEndereco.complemento();
+        this.bairro = dadosEndereco.bairro();
+        this.cep = dadosEndereco.cep();
+        this.cidade = dadosEndereco.cidade();
+        this.uf = dadosEndereco.uf();
+    }
 }
